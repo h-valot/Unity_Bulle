836a720fa3cfd02028d85ff4815b923395341229
hvalot-schl <109879255+h-valot@users.noreply.github.com>
2025-01-25 12:15:10 +0100

    Update character movement

--- Assets/_Content/_Scripts/Character/CharacterMotor.cs
+++ Assets/_Content/_Scripts/Character/CharacterMotor.cs
@@ -1,4 +1,5 @@
 using Sirenix.OdinInspector;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterMotor : MonoBehaviour
@@ -8,35 +9,80 @@ public class CharacterMotor : MonoBehaviour
 	[FoldoutGroup("Scriptable")][SerializeField] private SSO_Character m_ssoCharacter;
 	[FoldoutGroup("Scriptable")][SerializeField] private RSO_CurrentPosition m_rsoCurrentPosition;
 	[FoldoutGroup("Scriptable")][SerializeField] private RSE_Move m_rseMove;
+	[FoldoutGroup("Scriptable")][SerializeField] private RSE_Jump m_rseJump;
 
 	private Vector2 m_moveInput;
+	private bool m_isGrounded;
+	private float m_jumpTimer;
+
+	private bool IsJumping => m_jumpTimer > 0f;
 
 	private void OnEnable()
 	{
 		m_rseMove.Action += UpdateMoveInput;
+		m_rseJump.Action += UpdateJumpInput;
 	}
 
 	private void OnDisable()
 	{
-		m_rseMove.Action += UpdateMoveInput;
+		m_rseMove.Action -= UpdateMoveInput;
+		m_rseJump.Action -= UpdateJumpInput;
 	}
 
 	private void FixedUpdate()
 	{
-		Move();
+		CheckGround();
+
+		HandleJump();
+		HandleMove();
 
 		m_rsoCurrentPosition.Value = m_rigidbody2D.position;
 	}
 
 	private void UpdateMoveInput(Vector2 moveInput)
 	{
-		m_moveInput = moveInput;
+		m_moveInput = new Vector2(moveInput.x, 0f);
 	}
 
-	private void Move()
+	private void UpdateJumpInput(bool isPressed)
 	{
-		if (m_moveInput == Vector2.zero) return;
+		if (!m_isGrounded) return;
+		if (!isPressed) return;
 
-		m_rigidbody2D.MovePosition(m_rigidbody2D.position + m_moveInput * m_ssoCharacter.Speed * Time.fixedDeltaTime);
+		m_jumpTimer = m_ssoCharacter.JumpDuration;
+	}
+
+	private void CheckGround()
+	{
+		m_isGrounded = Physics2D.Raycast(
+			transform.position, 
+			Vector2.down,
+			m_ssoCharacter.GroundLength,
+			m_ssoCharacter.GroundLayerToInclude
+		);
+	}
+
+	private void HandleJump()
+	{
+		if (!IsJumping) return;
+
+		m_jumpTimer -= Time.fixedDeltaTime;
+	}
+
+	private void HandleMove()
+	{
+		Vector2 gravity = !m_isGrounded 
+			? Vector2.down * m_ssoCharacter.GravityForce * m_ssoCharacter.GravityScalar * Time.fixedDeltaTime 
+			: Vector2.zero;
+		
+		Vector2 input = m_moveInput != Vector2.zero 
+			? m_moveInput * m_ssoCharacter.Speed * Time.fixedDeltaTime 
+			: Vector2.zero;
+		
+		Vector2 jump = IsJumping
+			? Vector2.up * m_ssoCharacter.JumpForce * (m_jumpTimer / m_ssoCharacter.JumpDuration) * Time.fixedDeltaTime 
+			: Vector2.zero;
+
+		m_rigidbody2D.MovePosition(m_rigidbody2D.position + gravity + input + jump);
 	}
 }
---
+++ Assets/_Content/_Scripts/Scriptables/Runtime Scriptable/Input/RSE_Jump.cs
@@ -0,0 +1,5 @@
+using Scriptables;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_Jump", menuName = "Runtime Scriptable/Input/Jump")]
+public class RSE_Jump : RuntimeScriptableEvent<bool>{}
