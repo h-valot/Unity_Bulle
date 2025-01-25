4ebe4574423cae576cc61acb8b18112d5cec5a88
hvalot-schl <109879255+h-valot@users.noreply.github.com>
2025-01-25 09:44:30 +0100

    Add interactables

---
+++ Assets/_Content/_Scripts/Character/CharacterInteract.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class CharacterInteract : MonoBehaviour
+{
+	[FoldoutGroup("Scriptable")][SerializeField] private SSO_Character m_characterConfig;
+	[FoldoutGroup("Scriptable")][SerializeField] private RSE_Interact m_rseInteract;
+
+	public List<Interactable> m_interactables = new List<Interactable>();
+
+	private void OnEnable()
+	{
+		m_rseInteract.Action += Interact;
+	}
+
+	private void OnDisable()
+	{
+		m_rseInteract.Action -= Interact;
+	}
+
+	private void Interact()
+	{
+		// Assertion
+		if (m_interactables.Count <= 0) return;
+
+		m_interactables.FirstOrDefault()?.Interact();
+	}
+
+	/// <summary>
+	/// Add the given interactable into the interactable list.
+	/// </summary>
+	public void Add(Interactable interactable)
+	{
+		m_interactables.Add(interactable);
+	}
+
+	/// <summary>
+	/// Remove the given interactable from the interactable list.
+	/// The character will no longer be able to interact with it.
+	/// </summary>
+	public void Remove(Interactable interactable, bool doRecycle = false)
+	{
+		m_interactables.Remove(interactable);
+	}
+}
