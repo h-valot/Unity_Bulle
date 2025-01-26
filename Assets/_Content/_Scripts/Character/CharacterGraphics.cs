7cd14778e09008824661f22c3dd156e0f43610e4
hvalot-schl <109879255+h-valot@users.noreply.github.com>
2025-01-26 11:38:15 +0100

    Update character graphics

--- Assets/_Content/_Scripts/Character/CharacterGraphics.cs
+++ Assets/_Content/_Scripts/Character/CharacterGraphics.cs
@@ -1,11 +1,21 @@
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class CharacterGraphics : MonoBehaviour
 {
 	[FoldoutGroup("Internal references")][SerializeField] private GameObject m_divingSuit;
+	[FoldoutGroup("Internal references")][SerializeField] private SpriteRenderer m_spriteRenderer;
 
+	[FoldoutGroup("Scriptable")][SerializeField] private SSO_Character m_ssoCharacter;
 	[FoldoutGroup("Scriptable")][SerializeField] private RSO_ToggleDivingSuit m_rsoToggleDivingSuit;
+	[FoldoutGroup("Scriptable")][SerializeField] private RSE_Move m_rseMove;
+	[FoldoutGroup("Scriptable")][SerializeField] private RSO_CurrentItem m_rsoCurrentItem;
+
+	private Vector2 m_moveInput;
+	private float m_oscillationTimer;
+	private List<Sprite> m_currentSprites = new List<Sprite>();
+	private int m_currentIndex;
 
 	private void Start()
 	{
@@ -15,15 +25,55 @@ public class CharacterGraphics : MonoBehaviour
 	private void OnEnable()
 	{
 		m_rsoToggleDivingSuit.OnChanged += ToggleDivingSuit;
+		m_rseMove.Action += UpdateMoveInput;
 	}
 
 	private void OnDisable()
 	{
 		m_rsoToggleDivingSuit.OnChanged -= ToggleDivingSuit;
+		m_rseMove.Action -= UpdateMoveInput;
+	}
+
+	private void Update()
+	{
+		UpdateSprites();
+		HandleOscillation();
+	}
+
+	private void UpdateMoveInput(Vector2 input)
+	{
+		m_moveInput = input;
 	}
 
 	private void ToggleDivingSuit(bool isEnabled)
 	{
 		m_divingSuit.SetActive(isEnabled);
+	}
+
+	public void UpdateSprites()
+	{
+		if (m_moveInput.magnitude > 0f) m_spriteRenderer.flipX = m_moveInput.x > 0;
+
+		m_currentSprites = m_rsoCurrentItem.Value == ItemType.NONE
+			? m_ssoCharacter.WalkSprites
+			: m_moveInput.magnitude > 0f 
+				? m_ssoCharacter.CarryWalkSprites 
+				: m_ssoCharacter.CarrySprites;
+
+		m_spriteRenderer.sprite = m_currentSprites[m_currentIndex];
+	}
+
+	private void HandleOscillation()
+	{
+		m_oscillationTimer -= Time.deltaTime;
+		if (m_oscillationTimer <= 0f)
+		{
+			m_currentIndex++;
+			m_oscillationTimer = m_ssoCharacter.OscillationDuration;
+			if (m_currentIndex > m_currentSprites.Count - 1)
+			{
+				m_currentIndex = 0;
+			}
+		}
 	}
 }
--- Assets/_Content/_Scripts/Scriptables/Static Scriptable/SSO_Character.cs
+++ Assets/_Content/_Scripts/Scriptables/Static Scriptable/SSO_Character.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SSO_Character", menuName = "Static Scriptable/Character")]
@@ -17,4 +19,11 @@ public class SSO_Character : ScriptableObject
 	public float GroundLength;
 
 	public LayerMask GroundLayerToInclude;
+
+	[Title("Graphics")]
+	public float OscillationDuration;
+	public float CarryIdleDelay;
+	public List<Sprite> CarrySprites = new List<Sprite>();
+	public List<Sprite> CarryWalkSprites = new List<Sprite>();
+	public List<Sprite> WalkSprites = new List<Sprite>();
 }
