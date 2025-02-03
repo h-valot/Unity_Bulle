1df0e55e6de5de292054be8714fe1ac9345f5e3d
hvalot-schl <109879255+h-valot@users.noreply.github.com>
2025-02-03 13:54:58 +0100

    Fix bubbles

--- Assets/_Content/_Scripts/Interactable/LetterBoxTrigger.cs
+++ Assets/_Content/_Scripts/Interactable/LetterBoxTrigger.cs
@@ -8,18 +8,21 @@ public class LetterBoxTrigger : MonoBehaviour
 
 	[FoldoutGroup("Scriptable")][SerializeField] private RSE_SetBubble m_rseSetBubble;
 
+	private bool m_activated;
+
 	private void OnTriggerEnter2D(Collider2D collider)
 	{
-		// Assertion
+		// Assertions
 		if (!collider.TryGetComponent<CharacterMotor>(out var character)) return;
+		if (m_activated) return;
 
+		m_activated = true;
+		m_interactableFishermanDiving.IsValid = true;
 		m_rseSetBubble.Call(CharacterType.FISHERMAN, 0);
 		m_rseSetBubble.Call(CharacterType.FISHMONGER_SHORTKING, 0);
 		m_rseSetBubble.Call(CharacterType.FISHMONGER_TALL, 0);
 		m_rseSetBubble.Call(CharacterType.LETTER, 0);
 		m_rseSetBubble.Call(CharacterType.SINGER, 0);
 		m_rseSetBubble.Call(CharacterType.GRANDPA, 2);
-
-		m_interactableFishermanDiving.IsValid = true;
 	}
 }
